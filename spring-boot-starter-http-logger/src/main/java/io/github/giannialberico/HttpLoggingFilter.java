931923b81748bd89b4ceada4f074fad8e995dbec
Gianni Alberico <gianni.alberico@outlook.fr>
2026-03-13 14:12:55 +0100

    feat(starter): set up HttpLoggingFilter structure and dependencies

--- spring-boot-starter-http-logger/src/main/java/io/github/giannialberico/HttpLoggingFilter.java
+++ spring-boot-starter-http-logger/src/main/java/io/github/giannialberico/HttpLoggingFilter.java
@@ -1,4 +1,23 @@
 package io.github.giannialberico;
 
-public class HttpLoggingFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class HttpLoggingFilter extends OncePerRequestFilter {
+
+    HttpLoggerProperties httpLoggerProperties;
+
+    public HttpLoggingFilter(HttpLoggerProperties httpLoggerProperties) {
+        this.httpLoggerProperties = httpLoggerProperties;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        filterChain.doFilter(request, response);
+    }
 }
