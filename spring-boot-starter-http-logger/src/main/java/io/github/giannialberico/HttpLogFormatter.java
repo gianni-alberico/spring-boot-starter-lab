a2db26d9c6d967108a7ca4131aef1996eb030087
Gianni Alberico <gianni.alberico@outlook.fr>
2026-03-13 18:25:37 +0100

    feat(starter): complete HTTP logging starter
    - log requests/responses with method, status, headers, bodies, size, duration

---
+++ spring-boot-starter-http-logger/src/main/java/io/github/giannialberico/HttpLogFormatter.java
@@ -0,0 +1,138 @@
+package io.github.giannialberico;
+
+public class HttpLogFormatter {
+    private String title;
+    private String id;
+    private String method;
+    private String uri;
+    private String headers;
+    private String body;
+    private Integer status;
+    private String size;
+    private Long durationInMilli;
+
+    public HttpLogFormatter(Builder builder) {
+        this.title = builder.title;
+        this.id = builder.id;
+        this.method = builder.method;
+        this.uri = builder.uri;
+        this.headers = builder.headers;
+        this.body = builder.body;
+        this.status = builder.status;
+        this.size = builder.size;
+        this.durationInMilli = builder.durationInMilli;
+    }
+
+    public static class Builder {
+        private String title;
+        private String id;
+        private String method;
+        private String uri;
+        private String headers;
+        private String body;
+        private Integer status;
+        private String size;
+        private Long durationInMilli;
+
+        public Builder title(String title) {
+            this.title = title;
+            return this;
+        }
+
+        public Builder id(String id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder method(String method) {
+            this.method = method;
+            return this;
+        }
+
+        public Builder uri(String uri) {
+            this.uri = uri;
+            return this;
+        }
+
+        public Builder headers(String headers) {
+            this.headers = headers;
+            return this;
+        }
+
+        public Builder body(String body) {
+            this.body = body;
+            return this;
+        }
+
+        public Builder status(Integer status) {
+            this.status = status;
+            return this;
+        }
+
+        public Builder size(String size) {
+            this.size = size;
+            return this;
+        }
+
+        public Builder durationInMilli(Long durationInMilli) {
+            this.durationInMilli = durationInMilli;
+            return this;
+        }
+
+        public HttpLogFormatter build() {
+            return new HttpLogFormatter(this);
+        }
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setMethod(String method) {
+        this.method = method;
+    }
+
+    public void setUri(String uri) {
+        this.uri = uri;
+    }
+
+    public void setHeaders(String headers) {
+        this.headers = headers;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public void setDurationInMilli(Long durationInMilli) {
+        this.durationInMilli = durationInMilli;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        if (title != null)              sb.append("\n---------- ").append(title)  .append(" ----------\n");
+        if (id != null)                 sb.append("ID       : ").append(id)             .append("\n");
+        if (method != null)             sb.append("Method   : ").append(method)         .append("\n");
+        if (uri != null)                sb.append("URI      : ").append(uri)            .append("\n");
+        if (status != null)             sb.append("Status   : ").append(status)         .append("\n");
+        if (body != null)               sb.append("Body     : ").append(body)           .append("\n");
+        if (headers != null)            sb.append("Headers  : ").append(headers)        .append("\n");
+        if (size != null)               sb.append("Size     : ").append(size)           .append("\n");
+        if (durationInMilli != null)    sb.append("Duration : ").append(durationInMilli).append(" ms\n");
+
+        return sb.toString();
+    }
+}
--- spring-boot-starter-http-logger/src/main/java/io/github/giannialberico/HttpLoggingFilter.java
+++ spring-boot-starter-http-logger/src/main/java/io/github/giannialberico/HttpLoggingFilter.java
@@ -4,13 +4,21 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.ContentCachingResponseWrapper;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.List;
 
 public class HttpLoggingFilter extends OncePerRequestFilter {
 
-    HttpLoggerProperties httpLoggerProperties;
+    private final List<String> usefulHeaders = List.of("Content-Type", "Accept", "X-Correlation-ID", "User-Agent");
+    private final Logger log = LoggerFactory.getLogger(HttpLoggingFilter.class);
+    private final HttpLoggerProperties httpLoggerProperties;
 
     public HttpLoggingFilter(HttpLoggerProperties httpLoggerProperties) {
         this.httpLoggerProperties = httpLoggerProperties;
@@ -18,6 +26,106 @@ public class HttpLoggingFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        filterChain.doFilter(request, response);
+        long startTime = System.currentTimeMillis();
+
+        ContentCachingRequestWrapper wrappedRequest = new ContentCachingRequestWrapper(request, 1024);
+        ContentCachingResponseWrapper wrappedResponse = new ContentCachingResponseWrapper(response);
+
+        try {
+            filterChain.doFilter(wrappedRequest, wrappedResponse);
+        } finally {
+            long duration = System.currentTimeMillis() - startTime;
+
+            String requestBody = getBody(wrappedRequest.getContentAsByteArray(), wrappedRequest.getCharacterEncoding());
+            String responseBody = getBody(wrappedResponse.getContentAsByteArray(), wrappedResponse.getCharacterEncoding());
+
+            logRequest(wrappedRequest, requestBody);
+            logResponse(wrappedResponse, responseBody, duration);
+
+            wrappedResponse.copyBodyToResponse();
+        }
+    }
+
+    private void logRequest(HttpServletRequest request, String requestBody) {
+        HttpLogFormatter.Builder builder = new HttpLogFormatter.Builder()
+                .title("HTTP REQUEST")
+                .method(request.getMethod())
+                .uri(getFormattedURI(request))
+                .headers(getRequestHeaders(request));
+
+        if (httpLoggerProperties.includeRequestBody()) {
+            builder.body(requestBody);
+        }
+
+        log.info(builder.build().toString());
+    }
+
+    private void logResponse(ContentCachingResponseWrapper response, String responseBody, long duration) {
+        HttpLogFormatter.Builder builder = new HttpLogFormatter.Builder()
+                .title("HTTP RESPONSE")
+                .status(response.getStatus())
+                .headers(getResponseHeaders(response))
+                .durationInMilli(duration)
+                .size(formatSize(response.getContentAsByteArray().length));
+
+        if (httpLoggerProperties.includeResponseBody()) {
+            builder.body(responseBody);
+        }
+
+        log.info(builder.build().toString());
+    }
+
+    private String formatSize(int bytes) {
+        if (bytes < 1024) return bytes + " B";
+        int exp = (int) (Math.log(bytes) / Math.log(1024));
+        char pre = "KMG".charAt(exp - 1);
+        return String.format("%.1f %sB", bytes / Math.pow(1024, exp), pre);
+    }
+
+    private String getRequestHeaders(HttpServletRequest request) {
+        StringBuilder sb = new StringBuilder();
+
+        usefulHeaders.forEach(headerName -> {
+            String value = request.getHeader(headerName);
+            if (value != null) {
+                sb.append(headerName).append(": ").append(value).append("; ");
+            }
+        });
+        return sb.toString();
+    }
+
+    private String getResponseHeaders(HttpServletResponse response) {
+        StringBuilder sb = new StringBuilder();
+        var headerNames = response.getHeaderNames();
+
+        if (headerNames != null) {
+            for (String name : headerNames) {
+                String value = response.getHeader(name);
+                sb.append(name).append(": ").append(value).append(" | ");
+            }
+        }
+        return sb.toString();
+    }
+
+    private String getBody(byte[] buf, String charset) throws UnsupportedEncodingException {
+        if (buf.length > 0) {
+            return new String(buf, charset);
+        }
+
+        return null;
+    }
+
+    private String getFormattedURI(HttpServletRequest request) {
+        String uri = request.getRequestURI();
+        String queryString = request.getQueryString();
+
+        if (isStringEmpty(queryString)) {
+            return uri;
+        }
+        return uri + "?" + queryString;
+    }
+
+    private boolean isStringEmpty(String str) {
+        return str == null || str.isBlank();
     }
 }
