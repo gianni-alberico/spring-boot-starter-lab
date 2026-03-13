c1fb3097ce1e679ae279495dde03019d9e935d27
Gianni Alberico <gianni.alberico@outlook.fr>
2026-03-13 14:05:41 +0100

    chore: initial application setup from start.spring.io

---
+++ spring-boot-lab-app/src/main/java/io/github/giannialberico/spring_boot_lab_app/SpringBootLabAppApplication.java
@@ -0,0 +1,13 @@
+package io.github.giannialberico.spring_boot_lab_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLabAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLabAppApplication.class, args);
+	}
+
+}
---
+++ spring-boot-lab-app/src/test/java/io/github/giannialberico/spring_boot_lab_app/SpringBootLabAppApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.giannialberico.spring_boot_lab_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLabAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
