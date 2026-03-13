78656d97ce7bf1be3cb65fb83131a38e2318b76f
Gianni Alberico <gianni.alberico@outlook.fr>
2026-03-13 14:16:13 +0100

    feat(starter): implement conditional HttpLoggerAutoConfiguration

--- spring-boot-starter-http-logger/src/main/java/io/github/giannialberico/HttpLoggerAutoConfiguration.java
+++ spring-boot-starter-http-logger/src/main/java/io/github/giannialberico/HttpLoggerAutoConfiguration.java
@@ -1,4 +1,22 @@
 package io.github.giannialberico;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+@AutoConfiguration
+@EnableConfigurationProperties(HttpLoggerProperties.class)
+@ConditionalOnProperty(prefix = "http.logger", name = "enabled", havingValue = "true")
 public class HttpLoggerAutoConfiguration {
+
+    Logger log = LoggerFactory.getLogger(HttpLoggerAutoConfiguration.class);
+
+    @Bean
+    public HttpLoggingFilter httpLoggingFilter(HttpLoggerProperties httpLoggerProperties) {
+        log.info("registering http logger filter bean");
+        return new HttpLoggingFilter(httpLoggerProperties);
+    }
 }
