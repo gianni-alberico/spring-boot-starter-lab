9cc14f05eeb3152d0a3c093e297d209e662f621b
Gianni Alberico <gianni.alberico@outlook.fr>
2026-03-13 14:07:13 +0100

    feat(starter): add HttpLoggerProperties for starter configuration

--- spring-boot-starter-http-logger/src/main/java/io/github/giannialberico/HttpLoggerProperties.java
+++ spring-boot-starter-http-logger/src/main/java/io/github/giannialberico/HttpLoggerProperties.java
@@ -1,4 +1,20 @@
 package io.github.giannialberico;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.bind.DefaultValue;
+import org.springframework.boot.context.properties.bind.Name;
+
+@ConfigurationProperties("http.logger")
 public record HttpLoggerProperties(
+
+        @DefaultValue("false")
+        boolean enabled,
+
+        @Name("include-request-body")
+        @DefaultValue("false")
+        boolean includeRequestBody,
+
+        @Name("include-response-body")
+        @DefaultValue("false")
+        boolean includeResponseBody
 ) { }
